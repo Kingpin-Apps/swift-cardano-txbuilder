6383954d97843be5c2cda86f6ed943b5ebf2388b
Hareem Adderley <haddderley@kingpinapps.com>
2026-02-13 09:34:42 -0500

    fix: update core packages

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
             targets: ["SwiftCardanoTxBuilder"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", .upToNextMinor(from: "0.2.23")),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-chain.git", .upToNextMinor(from: "0.1.42")),
-        .package(url: "https://github.com/Kingpin-Apps/swift-ncal.git", .upToNextMinor(from: "0.2.1")),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.2.25"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-chain.git", from: "0.2.0"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-ncal.git", from: "0.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
